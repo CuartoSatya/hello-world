cf8589933ade02064e08feb4e0135218b7421e75
Cuarto Satya <kmxs@mx52.tiki.ne.jp>
2025-01-26 17:46:19 +0900

    Merge branch 'test' into main

---
+++ src/main/java/raisetech/student/management/ServletInitializer.java
@@ -0,0 +1,15 @@
+package raisetech.student.management;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+	}
