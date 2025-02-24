e4ec0dac9c2edbfcf944a3f3570c80378ca0e4ea
Cuarto Satya <kmxs@mx52.tiki.ne.jp>
2025-02-24 11:08:05 +0900

    Merge remote-tracking branch 'origin/main'

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,22 +1,37 @@
 package raisetech.student.management;
 
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+	private String name = "Oda Shintaro";
+	private String age = "44";
+
+	private Map<String,String> student;
+
 	public static void main(String[] args) {
 		//localhost:8080
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/RinkAcademy")
-	public String hello() {
-		return "I'm learning in Aviva Okayama School.";
-
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return name + " " + age + " years old";
+				}
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name, String age) {
+		this.name = name;
+		this.age = age;
+	}
+	@PostMapping("/studentName")
+	public void UpdateStudentName(String name) {
+		this.name = name;
 	}
  }
